dea88c18244dd2157e62c6d9e12ea6b1923eef19
MSSSKrishna <mssskrishna0734@gmail.com>
2024-05-24 15:24:01 +0530

    sign in page created

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
         <div className="mx-auto max-w-xl text-center">
           <h1 className="text-3xl font-extrabold sm:text-5xl">
             Manage your Expenses
-            <strong className="font-extrabold text-red-700 sm:block">
+            <strong className="font-extrabold text-primary sm:block">
               {" "}
               Increase Conversion.{" "}
             </strong>
@@ -19,7 +19,7 @@ function Hero() {
 
           <div className="mt-8 flex flex-wrap justify-center gap-4">
             <a
-              className="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+              className="block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-blue-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
               href="#"
             >
               Get Started
