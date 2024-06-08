43ba27aee53d6859daf9b7a6839e4aa5aaeef3b5
MSSSKrishna <mssskrishna0734@gmail.com>
2024-06-08 13:10:57 +0530

    budget page created check: sidenav image

---
+++ app/(routes)/dashboard/budgets/_components/CreateBudget.jsx
@@ -0,0 +1,118 @@
+"use client";
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter,
+  DialogClose,
+} from "../../../../../components/ui/dialog";
+import EmojiPicker from "emoji-picker-react";
+import { Button } from "../../../../../components/ui/button";
+import { Input } from "../../../../../components/ui/input";
+import { useUser } from "@clerk/nextjs";
+import { db } from "../../../../../utils/dbConfig";
+import { Budgets } from "../../../../../utils/schema";
+import { toast } from "sonner";
+
+function CreateBudget() {
+  const [emojiIcon, setemojiIcon] = useState("😀");
+  const [openEmojjPicker, setopenEmojiPicker] = useState(false);
+
+  const [name, setName] = useState();
+  const [amount, setAmount] = useState();
+  const { user } = useUser();
+
+  const createBudget = async () => {
+    const result = await db.insert(Budgets).values({
+      name: name,
+      amount: amount,
+      icon: emojiIcon,
+      createdBy: user.primaryEmailAddress.emailAddress,
+    });
+    if(result){
+      toast("New Budget Created")
+    }
+  };
+
+  return (
+    <div>
+      <Dialog>
+        <DialogTrigger asChild>
+          <div
+            className="flex flex-col cursor-pointer items-center 
+    border-dashed bg-slate-100 p-10 rounded-md hover:shadow-md "
+          >
+            <h2 className="text-3xl">+</h2>
+            <h2>Create Budget</h2>
+          </div>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Create new Budget</DialogTitle>
+            <DialogDescription>
+              <div className="mt-5">
+                <Button
+                  onClick={() => setopenEmojiPicker(!openEmojjPicker)}
+                  variant="outline"
+                  className="text-lg"
+                >
+                  {emojiIcon}
+                </Button>
+              </div>
+              <div className="absolute">
+                <EmojiPicker
+                  open={openEmojjPicker}
+                  onEmojiClick={(e) => {
+                    // console.log(e);
+                    setemojiIcon(e.emoji);
+                    setopenEmojiPicker(false);
+                  }}
+                />
+              </div>
+
+              <div className="mt-2">
+                <h2 className="text-black font-medium my-1">Budget Name</h2>
+                <Input
+                  placeholder="e.g. Car Petrol"
+                  className="text-black"
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                />
+              </div>
+              <div className="mt-2">
+                <h2 className="text-black font-medium my-1">Budget Amount</h2>
+                <Input
+                  placeholder="e.g. 5000 Rs"
+                  className="text-black"
+                  onChange={(e) => {
+                    setAmount(e.target.value);
+                  }}
+                />
+              </div>
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter className="sm:justify-start">
+            <DialogClose asChild>
+              <Button
+                className="mt-2 w-full"
+                disabled={!(name && amount)}
+                onClick={() => {
+                  createBudget();
+                }}
+              >
+                Create Budget
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
+
+export default CreateBudget;
--- app/(routes)/dashboard/layout.jsx
+++ app/(routes)/dashboard/layout.jsx
@@ -1,10 +1,34 @@
-import React from "react";
-// import SideNav from 'com'
+"use client";
 import SideNav from "./_components/SideNav";
 import DashboardHeader from "./_components/DashboardHeader";
 
+import { Budgets } from "../../../utils/schema";
+
+import { db } from "../../../utils/dbConfig";
+import { eq } from "drizzle-orm";
+import { useUser } from "@clerk/nextjs";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 function DashboardLayout({ children }) {
+  const { user } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    user && checkUserBudgets();
+  }, [user]);
+
+  const checkUserBudgets = async () => {
+    const result = await db
+      .select()
+      .from(Budgets)
+      .where(eq(Budgets.createdBy, user));
+    if (result?.length == 0) {
+      router.replace("/dashboard/budgets");
+    }
+    console.log(result);
+  };
+
   return (
     <div>
       <div className="fixed md:w-64 hidden md:block">
