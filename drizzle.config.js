ca2fcf56b682cf45e2881a258ca5cab8a440b8f8
MSSSKrishna <mssskrishna0734@gmail.com>
2024-06-08 13:12:03 +0530

    config files updated

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,8 +1,8 @@
 /** @type { import("drizzle-kit").Config } */
 export default {
-    schema: "./utils/schema.jsx",
-    dialect: 'postgresql',
-    dbCredentials: {
-      url: "postgresql://neondb_owner:j5RfevtkqLO1@ep-muddy-mode-a5gew9qs.us-east-2.aws.neon.tech/Expenses-Tracker?sslmode=require",
-    }
-  };
+  schema: "./utils/schema.jsx",
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: 'postgresql://neondb_owner:j5RfevtkqLO1@ep-muddy-mode-a5gew9qs.us-east-2.aws.neon.tech/Expenses-Tracker?sslmode=require',
+  }
+};  
--- utils/dbConfig.jsx
+++ utils/dbConfig.jsx
@@ -1,5 +1,6 @@
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
-import * as schema from './schema'
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "./schema";
 const sql = neon('postgresql://neondb_owner:j5RfevtkqLO1@ep-muddy-mode-a5gew9qs.us-east-2.aws.neon.tech/Expenses-Tracker?sslmode=require');
-const db = drizzle(sql,{schema});
+export const db = drizzle(sql, { schema });
+
